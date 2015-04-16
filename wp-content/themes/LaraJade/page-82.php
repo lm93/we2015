45c5129921345cac57331284e8b848183dd9f82b
wtq-git <nico.kurmann@gmail.com>
2015-04-16 03:06:42 +0200

    portfolio done

--- wp-content/themes/LaraJade/page-82.php
+++ wp-content/themes/LaraJade/page-82.php
@@ -14,18 +14,30 @@
             <figure class="blog_list__item__inner">
                 <figcaption>
                     <?php
-                            $a = array('p'=>$ID_blog_post); //TODO portfolio entries
-                            query_posts($a);                //todo
-                            the_post();
-                            $more=1;
-                            echo '<img src="';              //todo
-                            echo wp_get_attachment_image_src( get_post_thumbnail_id( $ID_blog_post ), 'single-post-thumbnail' )['0'];
-                            echo '" alt="">';
-                            echo '<strong>';
-                            echo get_the_title($ID_blog_post); //todo
-                            echo '</strong>';
-                            echo '<br><br>';
-                            the_content();
+                        $a = array('p'=>$ID_blog_post); //TODO portfolio entries
+                        query_posts($a);                //todo
+                        the_post();
+                        $more=1;
+                        echo '<img src="';              //todo
+                        echo wp_get_attachment_image_src( get_post_thumbnail_id( $ID_blog_post ), 'single-post-thumbnail' )['0'];
+                        echo '" alt="">';
+                        echo '<strong>';
+                        echo get_the_title($ID_blog_post); //todo
+                        echo '</strong>';
+                        echo '<br><br>';
+                        the_content();
+                        echo '<br><br>';
+                        echo '<h5>Description: </h5>'
+                        echo echo get_post_meta($_GET["ID"], 'description', true);
+
+                        echo '<br><br>';
+                        echo '<a href="';
+                        echo get_post_meta($_GET["ID"], 'website', true);
+                        
+                        echo '">to the website</a><br/>';
+                        echo '<a href="?page_id=6';
+                        echo '">back to the projects</a>';
+                            
 
                     ?>
                 </figcaption>
